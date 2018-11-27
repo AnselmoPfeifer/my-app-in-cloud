c569bae5d0ffad0a96f35809e96de9d330f83301
anselmopfeifer <anspfeifer@gmail.com>
2018-11-27 00:53:36 -0300

    Set the new info on terraform output

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,22 @@
-output "instance-one" {
-  value = ["${aws_instance.ec2-app-server-one.tags.Name}", "${aws_instance.ec2-app-server-one.public_dns}"]
+output "app-server-1" {
+  value = [
+    "${aws_instance.ec2-app-server-one.tags.Name}",
+    "${aws_instance.ec2-app-server-one.public_dns}",
+    "port=22"
+  ]
 }
 
-output "instance-one" {
-  value = ["${aws_instance.ec2-app-server-two.tags.Name}", "${aws_instance.ec2-app-server-two.public_dns}"]
+output "app-server-2" {
+  value = [
+    "${aws_instance.ec2-app-server-two.tags.Name}",
+    "${aws_instance.ec2-app-server-two.public_dns}",
+    "port=22"
+  ]
 }
 
-output "elb" {
-  value = "${aws_elb.app-server.dns_name}"
+output "public-access" {
+  value = [
+    "${aws_elb.app-server.dns_name}",
+    "port=80"
+  ]
 }
